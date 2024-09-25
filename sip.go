332a4f10364b98599af8981bdebec827e104d9f6
Henry <5065089+nguyengg@users.noreply.github.com>
2024-09-24 19:37:38 -0700

    ops: log timestamp when scale-in protection will be disabled

--- sip.go
+++ sip.go
@@ -100,7 +100,7 @@ mainLoop:
 					}
 				}
 				if s.IdleAtLeast > 0 {
-					s.Logger.Printf("all workers idle, will disable scale-in protection in %.4f seconds", s.IdleAtLeast.Seconds())
+					s.Logger.Printf("all workers idle, will disable scale-in protection at %s (in %.4f seconds)", time.Now().Add(s.IdleAtLeast).Format(time.RFC3339), s.IdleAtLeast.Seconds())
 					delay = time.NewTimer(s.IdleAtLeast)
 					continue mainLoop
 				}
